5e1a2b52304fcab3d500f92227ddd87f81cf00be
Viktor Tusa <tusa.balabit.hu>
2013-06-03 18:29:44 +0200

    Added simple editing capability to front-end

--- public/sctest.js
+++ public/sctest.js
@@ -1,4 +1,8 @@
 
+function destroy(val, arr) {
+    for (var i = 0; i < arr.length; i++) if (arr[i] === val) arr.splice(i, 1);
+}
+
 scmodule = angular.module('sctest', ['ui.bootstrap']);
 
 scmodule.controller("scload", function($scope, $http) {
@@ -13,4 +17,49 @@ scmodule.controller("scload", function($scope, $http) {
      
       $scope.ruleset_show = true;
    };
+   $scope.edit = function()
+   {
+     $scope.is_editing = true;
+   }
+
+   $scope.set = function()
+   {
+     $scope.is_editing = false;
+   }
+
+
+   $scope.addPattern = function(rules)
+   {
+     rules.push({ pattern : "" });
+   }
+
+   $scope.removePattern = function(rules, rule)
+   {
+     destroy(rule, rules);
+   }
+
 });
+
+/*scmodule.directive ('unfocus', function() { return {
+  restrict: 'A',
+  link: function (scope, element, attribs) {
+      
+    element[0].focus();
+      
+    element.bind ("blur", function() {
+        scope.$apply(attribs["unfocus"]);
+        //console.log("??");
+      });
+                                   
+  } 
+    
+ } 
+});
+
+function sccontrol($scope) {
+    $scope.name= "Freewind";
+
+    $scope.enableEdit = function() { $scope.edit = true; }
+    $scope.disableEdit = function() { $scope.edit = false;  }
+
+}*/
