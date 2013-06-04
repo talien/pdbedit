c47f86a18275ff7c05dc5ce514de9ceceb18d2d4
Viktor Tusa <tusa.balabit.hu>
2013-06-04 19:46:16 +0200

    Finished editing function, no save yet.

--- public/sctest.js
+++ public/sctest.js
@@ -1,3 +1,22 @@
+function uuid() {
+  var s = [], itoh = '0123456789abcedf';
+ 
+  // Make array of random hex digits. The UUID only has 32 digits in it, but we
+  // allocate an extra items to make room for the '-'s we'll be inserting.
+  for (var i = 0; i <36; i++) s[i] = Math.floor(Math.random()*0x10);
+ 
+  // Conform to RFC-4122, section 4.4
+  s[14] = 4;  // Set 4 high bits of time_high field to version
+  s[19] = (s[19] & 0x3) | 0x8;  // Specify 2 high bits of clock sequence
+ 
+  // Convert to hex chars
+  for (var i = 0; i <36; i++) s[i] = itoh[s[i]];
+ 
+  // Insert '-'s
+  s[8] = s[13] = s[18] = s[23] = '-';
+ 
+  return s.join('');
+}
 
 function destroy(val, arr) {
     for (var i = 0; i < arr.length; i++) if (arr[i] === val) arr.splice(i, 1);
@@ -16,6 +35,8 @@ scmodule.controller("scload", function($scope, $http) {
       });
      
       $scope.ruleset_show = true;
+      $scope.is_rule_editing = false;
+      $scope.is_editing = false;
    };
    $scope.edit = function()
    {
@@ -27,16 +48,53 @@ scmodule.controller("scload", function($scope, $http) {
      $scope.is_editing = false;
    }
 
+   $scope.edit_rule = function()
+   {
+     $scope.is_rule_editing = true;
+   }
+
+   $scope.set_rule = function()
+   {
+     $scope.is_rule_editing = false;
+   }
+
+   $scope.add_rule = function(rules)
+   {
+     rules.push( { provider: "", rule_class: "", patterns: Array(), tags: Array(), id: uuid() });
+     $scope.is_rule_editing = true;
+   }
 
    $scope.addPattern = function(rules)
    {
      rules.push({ pattern : "" });
    }
 
+   $scope.addTags = function(tags)
+   {
+     tags.push("");
+   }
+
    $scope.removePattern = function(rules, rule)
    {
      destroy(rule, rules);
    }
+
+   $scope.openEditor = function (pattern)
+   {
+     $scope.editing_pattern = pattern;
+     $scope.is_pattern_editor_open = true;
+   }
+
+   $scope.closeEditor = function ()
+   {
+     $scope.is_pattern_editor_open = false;
+   }
+
+   $scope.modal_opts = {
+      backdropFade: true,
+      dialogFade:true,
+      backdrop: true
+   };
 
 });
 
