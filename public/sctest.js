2e0e2f534d7ce4f5ceefaf024d6cb2cd17354ff9
Viktor Tusa <tusa@balabit.hu>
2013-06-05 02:07:50 +0200

    Added pattern editor & reworked buttons

--- public/sctest.js
+++ public/sctest.js
@@ -24,6 +24,52 @@ function destroy(val, arr) {
 
 scmodule = angular.module('sctest', ['ui.bootstrap']);
 
+var qstring_attribute_function = function(token)
+{
+   return token.charAt(0) + token.charAt(token.length-1);
+};
+
+var is_parser = function(token)
+{
+   return (token.length > 4) && (token.charAt(0) === "@") && (token.charAt(1) !== "@") && (token.charAt(token.length-1) === "@") && (token.charAt(token.length-2) !== "@");
+};
+
+var populate_parser_attributes_from_token = function(scope, token)
+{
+   var str = token.substring(1, token.length - 1 );
+   parts = str.split(":");
+   scope.selected_parser = { "name" : parts[0] };
+   scope.parser_variable = parts[1];
+   scope.parser_attributes = parts[2];
+};
+
+var tokenize = function (str)
+{
+   var items = str.split(" ");
+   var result = new Array();
+   for (i=0; i<items.length; i++)
+   {
+       result[i] = new Object();
+       result[i].value = items[i];
+       result[i].index = i;
+   }
+   return result;
+};
+
+var join = function(param)
+{
+   if (typeof(param) == 'undefined' || param == null) {
+        return '';
+   }
+   var res = new Array();
+   for (var i = 0; i < param.length; i++)
+   {
+       res[i] = param[i].value;
+   }
+   return res.join(' ');
+};
+
+
 scmodule.controller("scload", function($scope, $http) {
    $http.get("namelist").then(function(res) {
        $scope.test_data = res.data;
@@ -83,41 +129,83 @@ scmodule.controller("scload", function($scope, $http) {
    {
      $scope.editing_pattern = pattern;
      $scope.is_pattern_editor_open = true;
+     $scope.logelements = tokenize($scope.editing_pattern.pattern);
    }
 
-   $scope.closeEditor = function ()
+   $scope.cancelEditor = function ()
    {
      $scope.is_pattern_editor_open = false;
+   }
+
+   $scope.saveEditor = function()
+   {
+     $scope.is_pattern_editor_open = false;
+     $scope.editing_pattern.pattern = join($scope.logelements);
    }
 
    $scope.modal_opts = {
       backdropFade: true,
       dialogFade:true,
-      backdrop: true
+      backdrop: true,
+      dialogClass : "modal container span13"
    };
 
+    $scope.parser_select = [ {"name":"QSTRING"}, {"name":"ESTRING"}, {"name":"STRING"}];
+
+    $scope.test_change = function(model, evt)
+    {
+        var index = parseInt(evt.target.id.split("_")[1]);
+        $scope.editing = true;
+        $scope.index = index;
+        $scope.selected_parser = { "name":"" };
+        $scope.parser_variable = "";
+        $scope.token = $scope.logelements[index];
+        if (is_parser($scope.token.value))
+        {
+            populate_parser_attributes_from_token($scope, $scope.token.value);
+        };
+    };
+
+    $scope.save_item = function()
+    {
+        $scope.editing = false;
+        var res = Array();
+        var items = $scope.logelements;
+        var index = $scope.index;
+        for (var i=0;i<items.length; i++)
+        {
+           if (i != index)
+           {
+              res[i] = items[i];
+           }
+           else
+           {
+              res[i] = { "value": "@" + $scope.selected_parser.name + ":" + $scope.parser_variable+ ":" + $scope.parser_attributes + "@", "index" : index };
+           }
+        }
+        $scope.logelements = res;
+        $scope.parser_variable = "";
+        $scope.parser_attributes = "";
+    };
+
 });
 
-/*scmodule.directive ('unfocus', function() { return {
-  restrict: 'A',
-  link: function (scope, element, attribs) {
-      
-    element[0].focus();
-      
-    element.bind ("blur", function() {
-        scope.$apply(attribs["unfocus"]);
-        //console.log("??");
+
+scmodule.directive('logLine', function () {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attr, ngModelCtrl) {
+
+      ngModelCtrl.$formatters.unshift( function (valueFromModel) {
+        return join(valueFromModel);
       });
-                                   
-  } 
-    
- } 
+
+      ngModelCtrl.$parsers.push(function(valueFromInput) {
+        scope.show_click_on_tokens = true;
+        return tokenize(valueFromInput);
+      });
+
+    }
+  };
 });
 
-function sccontrol($scope) {
-    $scope.name= "Freewind";
-
-    $scope.enableEdit = function() { $scope.edit = true; }
-    $scope.disableEdit = function() { $scope.edit = false;  }
-
-}*/
